0c43fb8a5a71b303fc840f51c7964beaf11d4555
Seberyak <v_liparteliani@cu.edu.ge>
2021-01-17 01:12:09 +0400

    added user permission asserter

--- src/core/utils/asserts.ts
+++ src/core/utils/asserts.ts
@@ -1,4 +1,7 @@
 import { MError } from "./errors";
+import { IUser } from "../../../schemas/user/helper-schemas";
+import { ObjectId } from "bson";
+import { UserTypes } from "../../../schemas/user/user-types";
 
 export function assertResourceExist<T extends any>(
 	resource: T | null | undefined,
@@ -6,5 +9,20 @@ export function assertResourceExist<T extends any>(
 ): asserts resource is T {
 	if (typeof resource !== "boolean" && !resource) {
 		throw new MError(404, `${resourceType.toLocaleUpperCase()} not found`);
+	}
+}
+
+export function assertUserHasPermission<
+	T extends {
+		author: ObjectId;
+	}
+>(user: IUser, resource: T): void | never {
+	assertResourceExist(user, "user");
+	assertResourceExist(resource, "resource");
+	if (
+		!user._id.equals(resource.author) &&
+		!user.type.equals(UserTypes.Admin)
+	) {
+		throw new MError(403, "User has not permission");
 	}
 }
