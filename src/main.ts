7b0c00bae549398ac3f26fd76ae8f06f0ca3cccd
Valera Liparteliani <liparteliani12.vl@gmail.com>
2021-03-01 00:55:36 +0400

    debuging in production

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { AppModule } from "./app.module";
 import { ErrorFilter } from "./api/error-filter";
 import { NgrokService } from "./core/services/ngrok";
 require("dotenv").config();
+import * as fs from "fs";
+import path from "path";
 
 async function bootstrap() {
 	const apiPort = parseInt(process.env.API_PORT) || 3000;
 
-	const app = await NestFactory.create(AppModule);
+	const httpsOptions = {
+		key: fs.readFileSync("./secrets/private-key.pem"),
+		cert: fs.readFileSync(
+			"./home/valera/Documents/CarParts/secrets/public-certificate.pem"
+		),
+	};
+	const app = await NestFactory.create(AppModule, { httpsOptions });
 	app.useGlobalFilters(new ErrorFilter());
 	await app.listen(apiPort);
 	app.enableCors();
